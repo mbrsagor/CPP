ed94d188e16a629f7a21330a2c8f174e2842161c
Sagor <mbrsagor@gmail.com>
2020-12-01 23:24:46 +0600

    Use defaultdict example

--- effective/classes/index.py
+++ effective/classes/index.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class SimpleGradebook:
     def __init__(self):
         self._grades = {}
@@ -20,3 +23,33 @@ book.report_grade('Isaac Newton', 95)
 book.report_grade('Isaac Newton', 85)
 
 print(book.average_grade('Isaac Newton'))
+
+
+class BySubjectGradebook:
+    def __init__(self):
+        self._grades = {}  # Outer dict
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)  # Inner dict
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+        count += len(grades)
+        return total / count
+
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'Gym', 90)
+book.report_grade('Albert Einstein', 'Gym', 95)
+print(book.average_grade('Albert Einstein'))
