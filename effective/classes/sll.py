7c3c4bc9186befc435582377d75b6b3b1bc36900
Sagor <mbrsagor@gmail.com>
2020-12-02 02:20:13 +0600

    Link list

---
+++ effective/classes/sll.py
@@ -0,0 +1,36 @@
+class BinaryNode(object):
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+bar = [1, 2, 3]
+bar[0]
+
+
+class IndexableNode(BinaryNode):
+    def _traverse(self):
+        if self.left is not None:
+            yield from self.left._traverse()
+        yield self
+        if self.right is not None:
+            yield from self.right._traverse()
+
+    def __getitem__(self, index):
+        for i, item in enumerate(self._traverse()):
+            if i == index:
+                return item.value
+        raise IndexError(f'Index {index} is out of range')
+
+
+tree = IndexableNode(10, left=IndexableNode(5, left=IndexableNode(2), right=IndexableNode(6, right=IndexableNode(7))),
+                     right=IndexableNode(15, left=IndexableNode(11)))
+
+print('LRR is', tree.left.right.right.value)
+print('Index 0 is', tree[0])
+print('Index 1 is', tree[1])
+print('11 in the tree?', 11 in tree)
+print('17 in the tree?', 17 in tree)
+print('Tree is', list(tree))
