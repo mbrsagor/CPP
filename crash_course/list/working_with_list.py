2341b0bef065be2fd59857c03fc2fc5a8cdc6f0e
Sagor <mbrsagor@gmail.com>
2020-12-02 12:56:02 +0600

    List compreshimsim

---
+++ crash_course/list/working_with_list.py
@@ -0,0 +1,11 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:
+    print(f"{magician.title()} was the great trick!")
+
+li = [li2 ** 2 for li2 in range(1, 11)]
+print(li)
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+for player in players[:3]:
+    print(player.title())
