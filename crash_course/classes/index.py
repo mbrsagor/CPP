18dbe0dcd15509c9f34bd1559f37ae0436d43494
Sagor <mbrsagor@gmail.com>
2020-12-02 21:16:45 +0600

    OOP create simple example

---
+++ crash_course/classes/index.py
@@ -0,0 +1,23 @@
+class Dog(object):
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
+
+
+if __name__ == '__main__':
+    my_dog = Dog('Willie', 6)
+    print(f"My dog's name is : {my_dog.name}")
+    print(f"My dog is {my_dog.age} years old.")
+
+    my_dog.sit()
+    my_dog.roll_over()
