c57b3c08c1aea15a3f1ccb87becae05ee41d729d
Sagor <mbrsagor@gmail.com>
2020-12-05 22:41:29 +0600

    Fuleant python bulding dict

---
+++ FluentPython/dict/Built_in_types.py
@@ -0,0 +1,17 @@
+a = dict(one=1, two=2, three=3)
+print(a)
+
+b = {'one': 1, 'two': 2, 'three': 3}
+print(b)
+
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+print(c)
+
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+print(d)
+
+e = dict({'three': 3, 'one': 1, 'two': 2})
+print(e)
+
+abc = a == b == c == d == e
+print(abc)
