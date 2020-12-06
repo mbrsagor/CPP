a5a8021a6574aabc7f762cfc42267fcf4e181ed5
Sagor <mbrsagor@gmail.com>
2020-12-06 22:32:18 +0600

    Make average number function and calss based

---
+++ FluentPython/FunctionDecoratorsAndClosures/averager.py
@@ -0,0 +1,27 @@
+class Averager():
+    def __init__(self):
+        self.series = []
+
+    def __call__(self, new_value):
+        self.series.append(new_value)
+        total = sum(self.series)
+        return total / len(self.series)
+
+
+avg = Averager()
+print(avg(10))
+
+
+def make_averager():
+    series = []
+
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total / len(series)
+
+    return averager
+
+
+avg = make_averager()
+print(avg(12))
