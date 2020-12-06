624a2929da726668ac7065e9aa86cb61e09f713e
Sagor <mbrsagor@gmail.com>
2020-12-06 22:21:45 +0600

    Function Decorator and closures

---
+++ FluentPython/FunctionDecoratorsAndClosures/decorators101.py
@@ -0,0 +1,49 @@
+def deco(func):
+    def inner():
+        print('running inner()')
+
+    return inner
+
+
+@deco
+def target():
+    # print('running target()')
+    pass
+
+
+target()
+
+registry = []
+
+
+def register(func):
+    print('running register(%s)' % func)
+    registry.append(func)
+    return func
+
+
+@register
+def f1():
+    print('running f1()')
+
+
+@register
+def f2():
+    print('running f2()')
+
+
+@register
+def f3():
+    print('running f3()')
+
+
+def main():
+    print('running main()')
+    print('registry ->', registry)
+    f1()
+    f2()
+    f3()
+
+
+if __name__ == '__main__':
+    main()
