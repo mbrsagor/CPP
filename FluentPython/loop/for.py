aad8a0916e31bfecad7a8164b788388325c06621
Sagor <mbrsagor@gmail.com>
2020-12-21 01:09:45 +0600

    While loop example with odd event number

---
+++ FluentPython/loop/for.py
@@ -0,0 +1,9 @@
+for i in range(2, 10):
+    if i % 2 == 0:
+        print(f"The {i} is event number.")
+
+print("\n")
+
+for j in range(1, 10):
+    if j % 2 == 1:
+        print(f"The {j} is odd number.")
---
+++ FluentPython/loop/while.py
@@ -0,0 +1,7 @@
+num = 1
+
+while num < 10:
+    print(num)
+    if num == 3:
+        break
+    num += 1
