7cf6aa5b358440df79bd61a36fe10bc9aee50412
Sagor <mbrsagor@gmail.com>
2021-01-11 11:55:36 +0600

    List comprehensions

---
+++ review/list/list.py
@@ -0,0 +1,20 @@
+students = [
+    {
+        "name": "Jacob Martin",
+        "father name": "Ros Martin",
+        "Address": "123 Hill Street",
+    }, {
+        "name": "Angela Stevens",
+        "father name": "Robert Stevens",
+        "Address": "3 Upper Street London",
+    }, {
+        "name": "Ricky Smart",
+        "father name": "William Smart",
+        "Address": "Unknown",
+    }
+]
+
+
+names_list = [student['name'] for student in students]
+print(names_list)
+
