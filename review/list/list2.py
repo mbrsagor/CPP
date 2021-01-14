1d1ed9226ab8a9c444658308b5850be4d10f2655
Sagor <mbrsagor@gmail.com>
2021-01-15 00:17:54 +0600

    Yield generator basic example

--- review/list/list2.py
+++ review/list/list2.py
@@ -4,3 +4,15 @@ for n, name in enumerate(names):
     print(f"{n + 1}. {name}")
 else:
     print("Done")
+
+
+def myNumbers():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
+
+
+for number in myNumbers():
+    print(number)
